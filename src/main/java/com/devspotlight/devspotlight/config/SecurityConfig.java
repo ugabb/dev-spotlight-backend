2cbb96d996dabd544bfdeb826c5c510703aa5370
Gabriel Barros <ugabrieldev@gmail.com>
2023-11-22 13:43:03 -0300

    feat(config): SecurityConfig created to basic authentication

---
+++ src/main/java/com/devspotlight/devspotlight/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.devspotlight.devspotlight.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                                .requestMatchers("/login").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                                .anyRequest().authenticated()
+
+                ).httpBasic(withDefaults())
+                .formLogin(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
