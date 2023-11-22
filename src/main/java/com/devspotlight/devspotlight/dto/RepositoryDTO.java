19172553c8d0761b5f08b427d557243a298e37df
Gabriel Barros <ugabrieldev@gmail.com>
2023-11-22 13:45:06 -0300

    feat(DTO): DTOs created

---
+++ src/main/java/com/devspotlight/devspotlight/dto/RepositoryDTO.java
@@ -0,0 +1,28 @@
+package com.devspotlight.devspotlight.dto;
+
+import com.devspotlight.devspotlight.model.Technologies;
+import com.devspotlight.devspotlight.model.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RepositoryDTO {
+    @NotBlank
+    @NotNull
+    private String name;
+    @NotBlank
+    @NotNull
+    private String description;
+    @NotBlank
+    @NotNull
+    private String linkRepo;
+
+    private List<Technologies> technologies;
+    private Integer likes;
+    @NotBlank
+    private Long userId;
+
+}
--- src/main/java/com/devspotlight/devspotlight/dto/UserDTO.java
+++ src/main/java/com/devspotlight/devspotlight/dto/UserDTO.java
@@ -1,12 +1,32 @@
 package com.devspotlight.devspotlight.dto;
 
-import lombok.Data;
+import com.devspotlight.devspotlight.model.FavRepositories;
+import com.devspotlight.devspotlight.model.Followers;
+import com.devspotlight.devspotlight.model.Role;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class UserDTO {
 
-    private Long id;
 
     @NotBlank
+    private String name;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+    @NotBlank
+    private String githubProfileLink;
+    private String githubProfilePhoto;
+    private List<Followers> followers;
+    private List<FavRepositories> favoritesRepositories;
+    private Role role;
+
 }
