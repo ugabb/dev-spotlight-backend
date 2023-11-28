56f121a9e914f0db9a86d26cd263f26162dba15d
ugabb <gsb.alternativo@gmail.com>
2023-11-27 22:10:10 -0300

    feat(OAuth2LoginSucessHandler ): getting user data from github incomplete

--- src/main/java/com/devspotlight/devspotlight/config/Auth2LoginSuccessHandler.java
+++ src/main/java/com/devspotlight/devspotlight/config/Auth2LoginSuccessHandler.java
@@ -5,10 +5,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Component
 public class Auth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
@@ -16,6 +19,14 @@ public class Auth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSuc
     private String frontendUrl;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+        if ("github".equals(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId())){
+            DefaultOAuth2User principal = (DefaultOAuth2User) authentication.getPrincipal();
+            Map<String, Object> attributes = principal.getAttributes();
+        }
+
+
         this.setAlwaysUseDefaultTargetUrl(true);
         this.setDefaultTargetUrl(frontendUrl);
         super.onAuthenticationSuccess(request, response, authentication);
